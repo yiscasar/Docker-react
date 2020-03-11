5ecac9c4ea74b252042c06cb63a9d064d58b134d
someone <someone@someplace.com>
2020-03-11 18:30:48 +0200

    Added travis file

--- src/App.test.js
+++ src/App.test.js
@@ -4,12 +4,4 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
 });
